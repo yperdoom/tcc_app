0dc3d7dd0aba3c03605623d81b275a9ceb6d491a
Pedro Henrique Antunes Pinto <pedro.pinto@amo.delivery>
2023-11-29 19:58:08 -0300

    finish connection errors

--- lib/components/data_table_foods.dart
+++ lib/components/data_table_foods.dart
@@ -1,30 +1,54 @@
 import 'package:Yan/components/data_table_colums.dart';
 import 'package:Yan/components/data_table_rows.dart';
 import 'package:Yan/components/parse_double.dart';
+import 'package:Yan/components/text_style.dart';
 import 'package:flutter/material.dart';
 
 Widget dataTableFoods(var prescriptionReceived) {
+  double oldCalorie = prescriptionReceived['recommended_calorie'];
+  double oldCarbo = prescriptionReceived['recommended_carbohydrate'];
+  double oldProtein = prescriptionReceived['recommended_protein'];
+  double oldLipid = prescriptionReceived['recommended_lipid'];
+  double oldTotal = oldCalorie + oldCarbo + oldProtein + oldLipid;
+
+  double newCalorie = prescriptionReceived['meals'][0]['calorie'];
+  double newCarbo = prescriptionReceived['meals'][0]['carbohydrate'];
+  double newProtein = prescriptionReceived['meals'][0]['protein'];
+  double newLipid = prescriptionReceived['meals'][0]['lipid'];
+  double newTotal = newCalorie + newCarbo + newProtein + newLipid;
+
+  double firstRowSize = 14;
+  double firstColSize = 14;
+  FontWeight firstColFont = FontWeight.w800;
+  double moreColSize = 16;
+  FontWeight moreColFont = FontWeight.w800;
+
   var rows = [
-    {
-      "field": "Calorias",
-      "old": prescriptionReceived['recommended_calorie'],
-      "new": parseDouble(prescriptionReceived['meals'][0]['calorie'])
-    },
-    {
-      "field": "Carboidratos",
-      "old": prescriptionReceived['recommended_carbohydrate'],
-      "new": parseDouble(prescriptionReceived['meals'][0]['carbohydrate'])
-    },
-    {
-      "field": "Proteinas",
-      "old": prescriptionReceived['recommended_protein'],
-      "new": parseDouble(prescriptionReceived['meals'][0]['protein'])
-    },
-    {
-      "field": "Lipídios",
-      "old": prescriptionReceived['recommended_lipid'],
-      "new": parseDouble(prescriptionReceived['meals'][0]['lipid'])
-    }
+    <DataCell>[
+      celula('Calorias', firstColSize, firstColFont),
+      celula(parseDouble(oldCalorie), moreColSize, moreColFont),
+      celula(parseDouble(newCalorie), moreColSize, moreColFont)
+    ],
+    <DataCell>[
+      celula('Carboidratos', firstColSize, firstColFont),
+      celula(parseDouble(oldCarbo), moreColSize, moreColFont),
+      celula(parseDouble(newCarbo), moreColSize, moreColFont)
+    ],
+    <DataCell>[
+      celula('Calorias', firstColSize, firstColFont),
+      celula(parseDouble(oldProtein), moreColSize, moreColFont),
+      celula(parseDouble(newProtein), moreColSize, moreColFont)
+    ],
+    <DataCell>[
+      celula('Lipídios', firstColSize, firstColFont),
+      celula(parseDouble(oldLipid), moreColSize, moreColFont),
+      celula(parseDouble(newLipid), moreColSize, moreColFont)
+    ],
+    <DataCell>[
+      celula('Total', firstColSize, firstColFont),
+      celula(parseDouble(oldTotal), moreColSize, moreColFont),
+      celula(parseDouble(newTotal), moreColSize, moreColFont)
+    ]
   ];
 
   return DataTable(
@@ -32,7 +56,23 @@ Widget dataTableFoods(var prescriptionReceived) {
       'Nutrientes',
       'Recomendados',
       'Encontrados'
-    ]),
+    ], firstRowSize),
     rows: dataTableRows(rows),
+  );
+}
+
+DataCell celula(
+  String text,
+  double fontSize,
+  FontWeight fontWeight,
+) {
+  return DataCell(
+    Text(
+      text,
+      style: textStyle(
+        fontSize: fontSize,
+        fontWeight: fontWeight,
+      ),
+    ),
   );
 }
--- lib/components/data_table_rows.dart
+++ lib/components/data_table_rows.dart
@@ -1,18 +1,11 @@
-import 'package:Yan/components/text_style.dart';
 import 'package:flutter/material.dart';
 
 List<DataRow> dataTableRows(var rows) {
   List<DataRow> linhas = [];
 
   for (int i = 0; i < rows.length; i++) {
-    print(rows[i]['field']);
-
     linhas.add(DataRow(
-      cells: <DataCell>[
-        DataCell(Text(rows[i]['field'].toString(), style: textStyle(fontSize: 18.0))),
-        DataCell(Text(rows[i]['old'].toString(), style: textStyle(fontSize: 18.0, fontWeight: FontWeight.w400))),
-        DataCell(Text(rows[i]['new'].toString(), style: textStyle(fontSize: 18.0, fontWeight: FontWeight.w400))),
-      ],
+      cells: rows[i],
     ));
   }
 
