164412b9e893e3549175ba629007a63b82d07379
Pedro Henrique Antunes Pinto <pedro.pinto@amo.delivery>
2023-12-12 12:54:58 -0300

    update to git

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: false,
       title: 'APP TCC',
       // theme: ThemeData.light(),
       theme: ThemeData.dark(),
