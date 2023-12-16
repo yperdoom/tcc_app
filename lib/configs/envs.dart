efc037030038459b440f8127a3da7e39a5c5a8d8
Pedro Henrique Antunes Pinto <pedro.pinto@amo.delivery>
2023-12-16 10:53:01 -0300

    finish strings

--- lib/configs/envs.dart
+++ lib/configs/envs.dart
@@ -1,5 +1,5 @@
 class Envs {
-  static String prod = 'http://18.228.31.51:4030';
-  static String local = 'http://192.168.1.100:4030';
+  static String prod = 'https://{prod_env_access}';
+  static String local = 'http://127.0.0.1:4030';
   static String env = 'prod';
 }
